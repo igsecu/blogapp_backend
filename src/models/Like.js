b3809cbcb8bdb8f934e52151afda19d53a0b49a2
Ignacio Cunial <igsecu@hotmail.com>
2024-03-13 14:54:55 -0300

    Created Like database model

---
+++ src/models/Like.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+const db = require("../../db");
+
+const Like = db.define("like", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+});
+
+module.exports = Like;
