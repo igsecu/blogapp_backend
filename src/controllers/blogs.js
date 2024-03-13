2d7f9cc700f2ac0914a7bf3b6dab6aa7ee03a081
Ignacio Cunial <igsecu@hotmail.com>
2024-03-13 16:04:11 -0300

    Created getBlogAccountById() function

---
+++ src/controllers/blogs.js
@@ -0,0 +1,45 @@
+const { Op } = require("sequelize");
+
+const Blog = require("../models//Blog");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const Notification = require("../models/Notification");
+const Like = require("../models/Like");
+const BlogAccount = require("../models/BlogAccount");
+
+const { deleteImage } = require("../utils/cloudinary");
+
+// Get account by id
+const getBlogAccountById = async (id) => {
+  try {
+    const account = await BlogAccount.findByPk(id, {
+      attributes: [
+        "id",
+        "email",
+        "username",
+        "isBanned",
+        "isVerified",
+        "image",
+      ],
+    });
+
+    if (account) {
+      return {
+        id: account.id,
+        email: account.email,
+        username: account.username,
+        isBanned: account.isBanned,
+        isVerified: account.isVerified,
+        image: account.image,
+      };
+    }
+
+    return account;
+  } catch (error) {
+    throw new Error("Error trying to get an account by its id");
+  }
+};
+
+module.exports = {
+  getBlogAccountById,
+};
