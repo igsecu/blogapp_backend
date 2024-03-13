14448669e3f2f46ec0713c33d01d4c1e72d5d0a2
Ignacio Cunial <igsecu@hotmail.com>
2024-03-13 15:04:36 -0300

    Created router file

---
+++ src/routes/blogs.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const Blog = require("../models/Blog");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const Notification = require("../models/Notification");
+const Like = require("../models/Like");
+const BlogAccount = require("../models/BlogAccount");
+
+module.exports = router;
