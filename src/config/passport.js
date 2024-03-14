adbce8ca9204e378572980066a43639a8df64a84
Ignacio Cunial <igsecu@hotmail.com>
2024-03-14 15:48:38 -0300

    Created google strategy

--- src/config/passport.js
+++ src/config/passport.js
@@ -6,6 +6,8 @@ const bcrypt = require("bcryptjs");
 
 const { getBlogAccountById } = require("../controllers/blogs");
 
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 module.exports = (passport) => {
   passport.use(
     new LocalStrategy(
@@ -20,32 +22,39 @@ module.exports = (passport) => {
           });
           if (accountFound) {
             // Match password
-            bcrypt.compare(
-              password,
-              accountFound.password,
-              async (err, isMatch) => {
-                if (err) {
-                  return done(err, null);
-                }
-                if (isMatch) {
-                  const account = await getBlogAccountById(accountFound.id);
+            if (accountFound.type === "LOCAL") {
+              bcrypt.compare(
+                password,
+                accountFound.password,
+                async (err, isMatch) => {
+                  if (err) {
+                    return done(err, null);
+                  }
+                  if (isMatch) {
+                    const account = await getBlogAccountById(accountFound.id);
 
-                  if (account.isBanned === true) {
+                    if (account.isBanned === true) {
+                      return done(null, false, {
+                        statusCode: 400,
+                        msg: "This account is banned! Please contact the admin of the page...",
+                      });
+                    }
+
+                    return done(null, account);
+                  } else {
                     return done(null, false, {
                       statusCode: 400,
-                      msg: "This account is banned! Please contact the admin of the page...",
+                      msg: `Incorrect password!`,
                     });
                   }
-
-                  return done(null, account);
-                } else {
-                  return done(null, false, {
-                    statusCode: 400,
-                    msg: `Incorrect password!`,
-                  });
                 }
-              }
-            );
+              );
+            } else {
+              return done(null, false, {
+                statusCode: 400,
+                msg: `Incorrect password!`,
+              });
+            }
           } else {
             return done(null, false, {
               statusCode: 404,
@@ -58,9 +67,47 @@ module.exports = (passport) => {
       }
     )
   ),
-    passport.serializeUser((user, done) => {
-      done(null, user.id);
-    });
+    // Google Strategy
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: "/api/auth/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+          try {
+            const userFound = await BlogAccount.findOne({
+              where: {
+                email: profile.emails[0].value,
+              },
+            });
+
+            if (userFound) {
+              const account = await getBlogAccountById(userFound.id);
+
+              if (account.type === "GOOGLE") {
+                return done(null, account);
+              }
+            } else {
+              const accountCreated = await BlogAccount.create({
+                email: profile.emails[0].value,
+                type: "GOOGLE",
+              });
+              if (accountCreated) {
+                return done(null, accountCreated);
+              }
+            }
+          } catch (error) {
+            return done(error, null);
+          }
+        }
+      )
+    );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
   passport.deserializeUser(async (id, done) => {
     try {
