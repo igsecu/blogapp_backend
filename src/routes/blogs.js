a34ed9b6a32b58b87d11492e341376d0a5f098cf
Ignacio Cunial <igsecu@hotmail.com>
2024-03-18 15:40:10 -0300

    Created PUT /post/:id route

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -8,7 +8,11 @@ const Notification = require("../models/Notification");
 const Like = require("../models/Like");
 const BlogAccount = require("../models/BlogAccount");
 
-const { uploadProfileImage, deleteImage } = require("../utils/cloudinary");
+const {
+  uploadPostImage,
+  uploadProfileImage,
+  deleteImage,
+} = require("../utils/cloudinary");
 const fsExtra = require("fs-extra");
 const fileUpload = require("express-fileupload");
 
@@ -23,6 +27,8 @@ const {
   ensureAuthenticatedUser,
   validateImageSize,
   validateName,
+  validateText,
+  validateTitle,
 } = require("../utils/index");
 
 const {
@@ -32,6 +38,9 @@ const {
   updateIsVerifiedAccount,
   getBlogById,
   updateBlogName,
+  getPostById,
+  updatePostText,
+  updatePostTitle,
 } = require("../controllers/blogs");
 
 const bcrypt = require("bcryptjs");
@@ -47,6 +56,7 @@ const Token = require("../models/Token");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const uuid = require("uuid");
+const { post } = require("../..");
 
 // Github Callback
 router.get(
@@ -178,6 +188,147 @@ router.get("/logout", (req, res, next) => {
     });
   });
 });
+
+// Create new post
+router.post(
+  "/post",
+  ensureAuthenticatedUser,
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: `${__dirname}/../../uploads`,
+  }),
+  async (req, res, next) => {
+    const { title, text, blogId } = req.body;
+
+    try {
+      if (validateTitle(title)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: validateTitle(title),
+        });
+      }
+
+      if (validateText(text)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: validateText(text),
+        });
+      }
+
+      if (!blogId) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "blogId is missing",
+        });
+      }
+
+      if (!validateId(blogId)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: `blogId: ${blogId} - Invalid format!`,
+        });
+      }
+
+      const blog = await getBlogById(blogId);
+
+      if (!blog) {
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Blog with ID: ${blogId} not found!`,
+        });
+      }
+
+      if (blog.account.id !== req.user.id) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "This blog is not yours! You can not create a new post...",
+        });
+      }
+
+      if (blog.isBanned === true) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "This blog is banned! You can not create a post...",
+        });
+      }
+
+      if (req.files?.image) {
+        if (await validateFileType(req.files.image.tempFilePath)) {
+          const message = await validateFileType(req.files.image.tempFilePath);
+
+          await fsExtra.unlink(req.files.image.tempFilePath);
+
+          return res.status(400).json({
+            statusCode: 400,
+            msg: message,
+          });
+        }
+
+        if (await validateImageSize(req.files.image.tempFilePath)) {
+          const message = await validateImageSize(req.files.image.tempFilePath);
+
+          await fsExtra.unlink(req.files.image.tempFilePath);
+
+          return res.status(400).json({
+            statusCode: 400,
+            msg: message,
+          });
+        }
+
+        const result = await uploadPostImage(req.files.image.tempFilePath);
+
+        await fsExtra.unlink(req.files.image.tempFilePath);
+
+        const postCreated = await Post.create({
+          title,
+          text,
+          blogId,
+          image: result.secure_url,
+          image_id: result.public_id,
+        });
+
+        if (postCreated) {
+          await Notification.create({
+            blogAccountId: req.user.id,
+            text: `Your post ${postCreated.title} was created successfully!`,
+          });
+        }
+
+        const post = await getPostById(postCreated.id);
+
+        res.status(201).json({
+          statusCode: 201,
+          msg: "Post created successfully!",
+          data: post,
+        });
+      } else {
+        const postCreated = await Post.create({
+          title,
+          text,
+          blogId,
+        });
+
+        if (postCreated) {
+          await Notification.create({
+            blogAccountId: req.user.id,
+            text: `Your post ${postCreated.title} was created successfully!`,
+          });
+        }
+
+        const post = await getPostById(postCreated.id);
+
+        res.status(201).json({
+          statusCode: 201,
+          msg: "Post created successfully!",
+          data: post,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return next("Error trying to create new post");
+    }
+  }
+);
 
 // Create new blog
 router.post("/blog", ensureAuthenticatedUser, async (req, res, next) => {
@@ -575,6 +726,70 @@ router.post("/account", async (req, res, next) => {
     });
   } catch (error) {
     return next("Error trying to create a new account");
+  }
+});
+
+// Update post
+router.put("/post/:id", ensureAuthenticatedUser, async (req, res, next) => {
+  const { title, text } = req.query;
+  const { id } = req.params;
+
+  let postUpdated;
+
+  try {
+    if (!validateId(id)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `ID: ${id} - Invalid format!`,
+      });
+    }
+
+    const post = await getPostById(id);
+
+    if (!post) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Post with ID: ${id} not found!`,
+      });
+    }
+
+    if (post.blog.account.id !== req.user.id) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "You can not update a post that is not yours!",
+      });
+    }
+
+    if (post.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This post is banned! You can not update it...",
+      });
+    }
+
+    if (title) {
+      postUpdated = await updatePostTitle(id, title);
+    }
+
+    if (text) {
+      postUpdated = await updatePostText(id, text);
+    }
+
+    if (!title && !text) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "Query parameter is missing",
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      msg: "Post updated successfully!",
+      data: postUpdated,
+    });
+  } catch (error) {
+    console.log(error);
+    return next("Error trying to update post");
   }
 });
 
