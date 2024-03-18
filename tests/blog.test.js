24fca5aeb7eedc198990acd046780e430b6d8cd6
Ignacio Cunial <igsecu@hotmail.com>
2024-03-18 14:26:05 -0300

    Created PUT /blog/:id route tests

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -154,3 +154,71 @@ describe("POST /blog route -> create new blog", () => {
     expect(response.body.msg).toBe("You successfully logged out!");
   });
 });
+
+describe("PUT /blog/:id route -> update blog", () => {
+  it("it should return 401 status code -> not authorized", async () => {
+    const response = await request(app).put("/api/blog/1");
+    expect(response.status).toBe(401);
+    expect(response.body.msg).toBe("You are not authorized! Please login...");
+  });
+  it("it should return 200 status code -> user logged in", async () => {
+    const user = {
+      email: "user1@fakeapis.io",
+      password: "F4k3ap1s.io",
+    };
+
+    const response = await request(app).post("/api/login").send(user);
+    expect(response.status).toBe(200);
+    expect(response.body.msg).toBe("You logged in successfully");
+    cookie = response.headers["set-cookie"];
+  });
+  it("it should return 400 status code -> id invalid format", async () => {
+    const response = await request(app)
+      .put("/api/blog/1")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe("ID: 1 - Invalid format!");
+  });
+  it("it should return 404 status code -> blog not found", async () => {
+    const response = await request(app)
+      .put("/api/blog/dd7bc3e0-f9a9-4554-bd3c-d229629f8d2e")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(404);
+    expect(response.body.msg).toBe(
+      "Blog with ID: dd7bc3e0-f9a9-4554-bd3c-d229629f8d2e not found!"
+    );
+  });
+  it("it should return 400 status code -> blog not yours", async () => {
+    const response = await request(app)
+      .put(`/api/blog/${blog2_id}`)
+      .set("Cookie", cookie);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe(
+      "You can not update a blog that is not yours!"
+    );
+  });
+  it("it should return 400 status code -> name exists", async () => {
+    const response = await request(app)
+      .put(`/api/blog/${blog1_id}`)
+      .send({ name: "blog 2" })
+      .set("Cookie", cookie);
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe(
+      "Blog with name: blog 2 exists! Try with another one..."
+    );
+  });
+  it("it should return 200 status code -> blog updated", async () => {
+    const response = await request(app)
+      .put(`/api/blog/${blog1_id}`)
+      .send({ name: "New Name" })
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+  });
+  it("it should return a 200 status code -> logout process", async () => {
+    const response = await request(app)
+      .get("/api/logout")
+      .set("Cookie", cookie);
+    expect(response.status).toBe(200);
+    expect(response.body.msg).toBe("You successfully logged out!");
+  });
+});
