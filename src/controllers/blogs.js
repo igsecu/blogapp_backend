8984e1af11a3c80268c88b2517b446112ce63e04
Ignacio Cunial <igsecu@hotmail.com>
2024-03-18 14:25:23 -0300

    Created updateBlogName() function

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -124,9 +124,67 @@ const updateIsVerifiedAccount = async (id) => {
   }
 };
 
+// Get blog by id
+const getBlogById = async (id) => {
+  try {
+    const dbResult = await Blog.findByPk(id, {
+      attributes: ["id", "name", "isBanned"],
+      include: {
+        model: BlogAccount,
+        attributes: ["id", "email", "username", "isBanned"],
+      },
+    });
+
+    if (dbResult) {
+      return {
+        id: dbResult.id,
+        name: dbResult.name,
+        isBanned: dbResult.isBanned,
+        account: {
+          id: dbResult.blogAccount.id,
+          email: dbResult.blogAccount.email,
+          username: dbResult.blogAccount.username,
+          isBanned: dbResult.blogAccount.isBanned,
+        },
+      };
+    }
+
+    return dbResult;
+  } catch (error) {
+    throw new Error("Error trying to get a blog by its id");
+  }
+};
+
+// Update blog name
+const updateBlogName = async (id, name) => {
+  try {
+    const updatedBlog = await Blog.update(
+      {
+        name,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    if (updatedBlog[0] === 1) {
+      const blog = await getBlogById(id);
+
+      return blog;
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to update blog name!");
+  }
+};
+
 module.exports = {
   getBlogAccountById,
   updateUserImage,
   updateUsername,
   updateIsVerifiedAccount,
+  getBlogById,
+  updateBlogName,
 };
