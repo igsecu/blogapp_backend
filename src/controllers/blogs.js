d4899afb1a5b024ace443bbde4a10e07d77b2906
Ignacio Cunial <igsecu@hotmail.com>
2024-03-18 15:40:30 -0300

    Created updatePostTitle and updatePostText functions

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -155,6 +155,62 @@ const getBlogById = async (id) => {
   }
 };
 
+// Get post by id
+const getPostById = async (id) => {
+  try {
+    const result = await Post.findByPk(id, {
+      attributes: [
+        "id",
+        "title",
+        "text",
+        "isBanned",
+        "readers",
+        "comments_number",
+        "likes_number",
+        "image",
+      ],
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "name", "isBanned"],
+          include: {
+            model: BlogAccount,
+            attributes: ["id", "username", "email", "isBanned"],
+          },
+        },
+      ],
+    });
+
+    if (result) {
+      return {
+        id: result.id,
+        title: result.title,
+        text: result.text,
+        isBanned: result.isBanned,
+        readers: result.readers,
+        likes: result.likes_number,
+        comments: result.comments_number,
+        image: result.image,
+        blog: {
+          id: result.blog.id,
+          name: result.blog.name,
+          isBanned: result.blog.isBanned,
+          account: {
+            id: result.blog.blogAccount.id,
+            username: result.blog.blogAccount.username,
+            image: result.blog.blogAccount.image,
+            isBanned: result.blog.blogAccount.isBanned,
+          },
+        },
+      };
+    }
+
+    return result;
+  } catch (error) {
+    throw new Error("Error trying to get a post by its id");
+  }
+};
+
 // Update blog name
 const updateBlogName = async (id, name) => {
   try {
@@ -180,6 +236,56 @@ const updateBlogName = async (id, name) => {
   }
 };
 
+// Update post title
+const updatePostTitle = async (id, title) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    if (updatedPost[0] === 1) {
+      const post = await getPostById(id);
+
+      return post;
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to update post title!");
+  }
+};
+
+// Update post text
+const updatePostText = async (id, text) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        text,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    if (updatedPost[0] === 1) {
+      const post = await getPostById(id);
+
+      return post;
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to update post text!");
+  }
+};
+
 module.exports = {
   getBlogAccountById,
   updateUserImage,
@@ -187,4 +293,7 @@ module.exports = {
   updateIsVerifiedAccount,
   getBlogById,
   updateBlogName,
+  getPostById,
+  updatePostText,
+  updatePostTitle,
 };
