fb5544e28c5231352244ff0ecc64bcae533053de
Ignacio Cunial <igsecu@hotmail.com>
2024-03-19 16:46:01 -0300

    Created PUT /account/:id/banned/true route

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1010,6 +1010,53 @@ router.post("/account", async (req, res, next) => {
   }
 });
 
+// Ban account
+router.put(
+  "/account/:id/banned/true",
+  ensureAuthenticatedAdmin,
+  async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+      if (!validateId(id)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: `ID: ${id} - Invalid format!`,
+        });
+      }
+
+      const accountFound = await getBlogAccountById(id);
+
+      if (!accountFound) {
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Account with ID: ${id} not found!`,
+        });
+      }
+
+      const updatedAccount = await BlogAccount.update(
+        {
+          isBanned: true,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+
+      if (updatedAccount) {
+        return res.status(200).json({
+          statusCode: 200,
+          msg: "Updated account successfully!",
+        });
+      }
+    } catch (error) {
+      return next("Error trying to ban an account");
+    }
+  }
+);
+
 // Update post image
 router.put(
   "/post/:id/image",
