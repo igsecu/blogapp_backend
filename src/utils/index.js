58db05da5be063660b84e6928dc2aaf97314c902
Ignacio Cunial <igsecu@hotmail.com>
2024-03-19 18:59:53 -0300

    Updated ensureAutheticatedAdmin() function

--- src/utils/index.js
+++ src/utils/index.js
@@ -179,12 +179,12 @@ const ensureAuthenticatedUser = (req, res, next) => {
 // Access Control Admin
 const ensureAuthenticatedAdmin = (req, res, next) => {
   if (req.isAuthenticated()) {
-    if (req.user.type === "ADMIN") {
+    if (req.user.isAdmin === true) {
       next();
     } else {
       return res.status(401).json({
         statusCode: 401,
-        msg: `You are not authorized! Please login with a User account...`,
+        msg: `You are not authorized! Please login with an Admin account...`,
       });
     }
   } else {
