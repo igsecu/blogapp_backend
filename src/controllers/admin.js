d110b23e41ce812d737988e4ef1f765e1ebf57a2
Ignacio Cunial <igsecu@hotmail.com>
2024-03-20 18:42:28 -0300

    Created getNotBannedPostsPagination function

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -692,6 +692,143 @@ const getBannedPostsPagination = async (id, page, limit) => {
   }
 };
 
+// Get not banned posts
+const getNotBannedPosts = async (id) => {
+  const results = [];
+  try {
+    const dbResults = await Post.findAll({
+      attributes: [
+        "id",
+        "title",
+        "text",
+        "isBanned",
+        "readers",
+        "comments_number",
+        "likes_number",
+        "image",
+      ],
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "name", "isBanned"],
+          include: {
+            model: BlogAccount,
+            attributes: ["id", "username", "email", "isBanned"],
+            where: {
+              id: {
+                [Op.not]: id,
+              },
+            },
+          },
+        },
+      ],
+      where: {
+        isBanned: false,
+      },
+    });
+
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          title: r.title,
+          text: r.text,
+          isBanned: r.isBanned,
+          readers: r.readers,
+          likes: r.likes_number,
+          comments: r.comments_number,
+          image: r.image,
+          blog: {
+            id: r.blog.id,
+            name: r.blog.name,
+            isBanned: r.blog.isBanned,
+            account: {
+              id: r.blog.blogAccount.id,
+              username: r.blog.blogAccount.username,
+              image: r.blog.blogAccount.image,
+              isBanned: r.blog.blogAccount.isBanned,
+            },
+          },
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get banned posts");
+  }
+};
+
+// Get not banned posts pagination
+const getNotBannedPostsPagination = async (id, page, limit) => {
+  const results = [];
+  try {
+    const dbResults = await Post.findAll({
+      attributes: [
+        "id",
+        "title",
+        "text",
+        "isBanned",
+        "readers",
+        "comments_number",
+        "likes_number",
+        "image",
+      ],
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "name", "isBanned"],
+          include: {
+            model: BlogAccount,
+            attributes: ["id", "username", "email", "isBanned"],
+            where: {
+              id: {
+                [Op.not]: id,
+              },
+            },
+          },
+        },
+      ],
+      where: {
+        isBanned: false,
+      },
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          title: r.title,
+          text: r.text,
+          isBanned: r.isBanned,
+          readers: r.readers,
+          likes: r.likes_number,
+          comments: r.comments_number,
+          image: r.image,
+          blog: {
+            id: r.blog.id,
+            name: r.blog.name,
+            isBanned: r.blog.isBanned,
+            account: {
+              id: r.blog.blogAccount.id,
+              username: r.blog.blogAccount.username,
+              image: r.blog.blogAccount.image,
+              isBanned: r.blog.blogAccount.isBanned,
+            },
+          },
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get not banned posts");
+  }
+};
+
 module.exports = {
   getBlogs,
   getBlogsPagination,
@@ -707,4 +844,6 @@ module.exports = {
   getPostsPagination,
   getBannedPosts,
   getBannedPostsPagination,
+  getNotBannedPosts,
+  getNotBannedPostsPagination,
 };
