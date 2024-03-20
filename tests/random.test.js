9808dc84e88240c46e93037dd303bad444c391f1
Ignacio Cunial <igsecu@hotmail.com>
2024-03-20 15:45:15 -0300

    Created PUT /account /blog /post route tests

--- tests/random.test.js
+++ tests/random.test.js
@@ -84,15 +84,7 @@ describe("POST /blog route -> create new blog", () => {
   }
 });
 
-let blogs;
 
-describe("GET /blogs route -> get all blogs", () => {
-  it("it should return 200 status code", async () => {
-    const response = await request(app).get("/api/blogs");
-    expect(response.status).toBe(200);
-    blogs = response.body.data;
-  });
-});
 
 describe("POST /post route -> create new post", () => {
   for (let x = 1; x <= 1000; x++) {
@@ -132,6 +124,16 @@ describe("POST /post route -> create new post", () => {
   }
 }); */
 
+let blogs;
+
+describe("GET /blogs route -> get all blogs", () => {
+  it("it should return 200 status code", async () => {
+    const response = await request(app).get("/api/blogs");
+    expect(response.status).toBe(200);
+    blogs = response.body.data;
+  });
+});
+
 let posts;
 
 describe("GET /posts route -> get all posts", () => {
@@ -140,6 +142,17 @@ describe("GET /posts route -> get all posts", () => {
     expect(response.status).toBe(200);
     posts = response.body.data;
     console.log(posts);
+  });
+});
+
+let accounts;
+
+describe("GET /posts route -> get all accounts", () => {
+  it("it should return 200 status code", async () => {
+    const response = await request(app).get("/api/accounts");
+    expect(response.status).toBe(200);
+    accounts = response.body.data;
+    console.log(accounts);
   });
 });
 
@@ -176,7 +189,7 @@ describe("GET /posts route -> get all posts", () => {
   }
 }); */
 
-describe("POST /comment route -> create new comment", () => {
+/* describe("POST /comment route -> create new comment", () => {
   for (let x = 2; x <= 500; x++) {
     const number = Math.floor(Math.random() * (20 - 1) + 1);
 
@@ -209,3 +222,115 @@ describe("POST /comment route -> create new comment", () => {
     });
   }
 });
+ */
+
+/* describe("POST /api/account/admin route -> create new user", () => {
+  it("it should return 201 status code -> create new account successfully", async () => {
+    const user = {
+      email: "admin@fakeapis.io",
+      password: "F4k3ap1s.io",
+      password2: "F4k3ap1s.io",
+    };
+
+    const response = await request(app).post("/api/account/admin").send(user);
+
+    expect(response.status).toBe(201);
+    expect(response.body.msg).toBe("Account created successfully!");
+    account1_id = response.body.data.id;
+  });
+});
+
+describe("PUT /api/account/:id/banned/true -> ban account", () => {
+  for (let x = 0; x < 2; x++) {
+    const position = Math.floor(Math.random() * 20);
+
+    it("it should return a 200 status code -> user logged in", async () => {
+      const user = {
+        email: "admin@fakeapis.io",
+        password: "F4k3ap1s.io",
+      };
+
+      const response = await request(app).post("/api/login").send(user);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You logged in successfully");
+      cookie = response.headers["set-cookie"];
+    });
+    it("it should return 200 status code -> ban account", async () => {
+      const response = await request(app)
+        .put(`/api/account/${accounts[position].id}/banned/true`)
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+    });
+    it("it should return a 200 status code -> logout process", async () => {
+      const response = await request(app)
+        .get("/api/logout")
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You successfully logged out!");
+    });
+  }
+});
+ */
+
+/* describe("PUT /api/blog/:id/banned/true -> ban blog", () => {
+  for (let x = 0; x < 5; x++) {
+    const position = Math.floor(Math.random() * 50);
+
+    it("it should return a 200 status code -> user logged in", async () => {
+      const user = {
+        email: "admin@fakeapis.io",
+        password: "F4k3ap1s.io",
+      };
+
+      const response = await request(app).post("/api/login").send(user);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You logged in successfully");
+      cookie = response.headers["set-cookie"];
+    });
+    it("it should return 200 status code -> ban account", async () => {
+      const response = await request(app)
+        .put(`/api/blog/${blogs[position].id}/banned/true`)
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+    });
+    it("it should return a 200 status code -> logout process", async () => {
+      const response = await request(app)
+        .get("/api/logout")
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You successfully logged out!");
+    });
+  }
+}); */
+
+/* describe("PUT /api/post/:id/banned/true -> ban post", () => {
+  for (let x = 0; x < 3; x++) {
+    const position = Math.floor(Math.random() * 50);
+
+    it("it should return a 200 status code -> user logged in", async () => {
+      const user = {
+        email: "admin@fakeapis.io",
+        password: "F4k3ap1s.io",
+      };
+
+      const response = await request(app).post("/api/login").send(user);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You logged in successfully");
+      cookie = response.headers["set-cookie"];
+    });
+    it("it should return 200 status code -> ban account", async () => {
+      const response = await request(app)
+        .put(`/api/post/${posts[position].id}/banned/true`)
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+    });
+    it("it should return a 200 status code -> logout process", async () => {
+      const response = await request(app)
+        .get("/api/logout")
+        .set("Cookie", cookie);
+      expect(response.status).toBe(200);
+      expect(response.body.msg).toBe("You successfully logged out!");
+    });
+  }
+});
+ */
