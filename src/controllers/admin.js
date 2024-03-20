dfbfc9501f89850d81c18ce87198d879bf007f76
Ignacio Cunial <igsecu@hotmail.com>
2024-03-20 18:09:30 -0300

    Created getBlogsAuthPagination function

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -262,6 +262,83 @@ const getNotBannedBlogsPagination = async (id, page, limit) => {
   }
 };
 
+// Get all blogs Auth
+const getBlogsAuth = async (id) => {
+  const results = [];
+  try {
+    const dbResults = await Blog.findAll({
+      attributes: ["id", "name", "isBanned"],
+      include: {
+        model: BlogAccount,
+        attributes: ["id", "email", "username", "isBanned"],
+        where: {
+          id,
+        },
+      },
+    });
+
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          name: r.name,
+          isBanned: r.isBanned,
+          account: {
+            id: r.blogAccount.id,
+            email: r.blogAccount.email,
+            username: r.blogAccount.username,
+            isBanned: r.blogAccount.isBanned,
+          },
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get all blogs");
+  }
+};
+
+// Get all blogs Auth pagination
+const getBlogsAuthPagination = async (id, page, limit) => {
+  const results = [];
+  try {
+    const dbResults = await Blog.findAll({
+      attributes: ["id", "name", "isBanned"],
+      include: {
+        model: BlogAccount,
+        attributes: ["id", "email", "username", "isBanned"],
+        where: {
+          id,
+        },
+      },
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (dbResults) {
+      dbResults.forEach((r) => {
+        results.push({
+          id: r.id,
+          name: r.name,
+          isBanned: r.isBanned,
+          account: {
+            id: r.blogAccount.id,
+            email: r.blogAccount.email,
+            username: r.blogAccount.username,
+            isBanned: r.blogAccount.isBanned,
+          },
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get all blogs");
+  }
+};
+
 module.exports = {
   getBlogs,
   getBlogsPagination,
@@ -269,4 +346,6 @@ module.exports = {
   getBannedBlogsPagination,
   getNotBannedBlogs,
   getNotBannedBlogsPagination,
+  getBlogsAuth,
+  getBlogsAuthPagination,
 };
