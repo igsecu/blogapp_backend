57d81d3d584994c317df51cffbbe4aa22f4d351a
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 16:49:59 -0300

    Imported admin router -> specified admin router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const blogRouter = require("./blogs");
 const usersRouter = require("./users");
 const loginRouter = require("./login");
+const adminRouter = require("./admin");
 
 // Specify routers root routes
 router.use("/", loginRouter);
 router.use("/users", usersRouter);
+router.use("/admin", adminRouter);
 /* router.use("/api", blogRouter); */
 
 module.exports = router;
