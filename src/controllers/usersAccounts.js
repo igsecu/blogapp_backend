fd95cf7be452899e105f37b40ef3820e4cfef0a9
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 16:07:27 -0300

    Updated getAccountById function -> removed type attribute

--- src/controllers/usersAccounts.js
+++ src/controllers/usersAccounts.js
@@ -115,86 +115,6 @@ const createAccount = async (req, res, next) => {
   }
 };
 
-// Create new user account from Github or Google
-const createAccountFromGithubOrGoogle = async (req, res, next) => {
-  const { email, password, password2 } = req.body;
-  try {
-    // Validations
-    if (validateEmail(email)) {
-      return res.status(400).json({
-        statusCode: 400,
-        msg: validateEmail(email),
-      });
-    }
-
-    const emailExist = await usersAccountsServices.checkEmailExists(email);
-
-    if (emailExist) {
-      return res.status(400).json({
-        statusCode: 400,
-        msg: `Email "${email}" exists! Try with another one!`,
-      });
-    }
-
-    if (validatePassword(password)) {
-      return res.status(400).json({
-        statusCode: 400,
-        msg: validatePassword(password),
-      });
-    }
-
-    if (validatePasswordConfirmation(password, password2)) {
-      return res.status(400).json({
-        statusCode: 400,
-        msg: validatePasswordConfirmation(password, password2),
-      });
-    }
-
-    // Hash Password
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        if (err) {
-          return next("Error trying to create a new account");
-        }
-        try {
-          const accountCreated =
-            await usersAccountsServices.createAccountFromGithubOrGoogle(
-              hash,
-              email
-            );
-
-          if (accountCreated) {
-            const account = await usersAccountsServices.getAccountById(
-              accountCreated.id
-            );
-
-            const url = `${process.env.URL}/api/users/account/${account.id}/verify`;
-
-            //await emailsServices.sendEmailVerification(url, account.email);
-
-            await notificationsServices.createNotification(
-              account.id,
-              "Your account was created successfully!"
-            );
-
-            return res.status(201).json({
-              statusCode: 201,
-              data: account,
-              msg: "Account created successfully!",
-            });
-          }
-        } catch (error) {
-          console.log(error.message);
-          return next("Error trying to create a new account");
-        }
-      });
-    });
-  } catch (error) {
-    console.log(error);
-    return next("Error trying to create a new account");
-  }
-};
-
 // Login process
 const login = async (req, res, next) => {
   if (req.user) {
@@ -283,10 +203,99 @@ const logout = async (req, res, next) => {
   });
 };
 
+// Google Authentication
+const authenticateGoogle = async () => {
+  await passport.authenticate("google", { scope: ["profile", "email"] });
+};
+
+// Google Callback
+const googleCallback = async (req, res, next) => {
+  if (req.user) {
+    const accountFound = await usersAccountsServices.getAccountById(
+      req.user.id
+    );
+
+    if (accountFound.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This account is banned! Please contact the admin of the page...",
+      });
+    }
+
+    if (accountFound.isVerified === false) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "Please verify your account!",
+      });
+    }
+
+    return res.status(200).json({
+      statusCode: 200,
+      msg: "You logged in successfully",
+      data: accountFound,
+    });
+  } else {
+    return next("Error trying to authenticate with Google");
+  }
+};
+
+// Verify Account
+const verifyAccount = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    if (!validateId(id)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `ID: ${id} - Invalid format!`,
+      });
+    }
+
+    const accountFound = await usersAccountsServices.getAccountById(id);
+
+    if (!accountFound) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Account with ID: ${id} not found!`,
+      });
+    }
+
+    if (accountFound.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This account is banned! You can not verify it...",
+      });
+    }
+
+    if (accountFound.isVerified === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This account is already verified!",
+      });
+    }
+
+    const updatedAccount = await usersAccountsServices.updateIsVerifiedAccount(
+      id
+    );
+
+    if (updatedAccount) {
+      return res.status(200).json({
+        statusCode: 200,
+        msg: "Your account is now verified!",
+        data: updatedAccount,
+      });
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   createAccount,
   login,
   getLoggedInAccount,
   logout,
-  createAccountFromGithubOrGoogle,
+  authenticateGoogle,
+  googleCallback,
+  verifyAccount,
 };
