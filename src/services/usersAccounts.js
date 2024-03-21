5a9ae8525016322a9fa747f5699eae76e2a810bf
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 11:42:18 -0300

    Created checkEmailExists, createAccount and getAccountById functions

---
+++ src/services/usersAccounts.js
@@ -0,0 +1,73 @@
+const BlogAccount = require("../models/BlogAccount");
+
+// Check if email exists
+const checkEmailExists = async (email) => {
+  try {
+    const emailFound = await BlogAccount.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return emailFound;
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to check if email exists");
+  }
+};
+
+// Create account
+const createAccount = async (hash, email) => {
+  try {
+    const accountCreated = await BlogAccount.create({
+      password: hash,
+      email: email.toLowerCase(),
+      type: "LOCAL",
+    });
+
+    return accountCreated;
+  } catch (error) {
+    throw new Error("Error trying to create new account");
+  }
+};
+
+// Get account by id
+const getAccountById = async (id) => {
+  try {
+    const account = await BlogAccount.findByPk(id, {
+      attributes: [
+        "id",
+        "email",
+        "username",
+        "isBanned",
+        "isVerified",
+        "image",
+        "type",
+        "isAdmin",
+      ],
+    });
+
+    if (account) {
+      return {
+        id: account.id,
+        email: account.email,
+        username: account.username,
+        isBanned: account.isBanned,
+        isVerified: account.isVerified,
+        image: account.image,
+        type: account.type,
+        isAdmin: account.isAdmin,
+      };
+    }
+
+    return account;
+  } catch (error) {
+    throw new Error("Error trying to get an account by its id");
+  }
+};
+
+module.exports = {
+  checkEmailExists,
+  createAccount,
+  getAccountById,
+};
