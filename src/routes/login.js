95ae2450f2d0a74a2ba447806794d8b769ca990b
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 16:32:43 -0300

    Created login.js router file

---
+++ src/routes/login.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const router = express.Router();
+
+const passport = require("passport");
+
+const usersAccountsController = require("../controllers/usersAccounts");
+
+// Login process
+const login = async (req, res, next) => {
+  if (req.user) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "An user is already logged in!",
+    });
+  }
+
+  const { email, password } = req.body;
+
+  if (validateEmail(email)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validateEmail(email),
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "Password is missing",
+    });
+  }
+
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      return next(error);
+    }
+    if (!user) {
+      if (info.statusCode === 400) {
+        return res.status(400).json({
+          statusCode: info.statusCode,
+          msg: info.msg,
+        });
+      }
+      return res.status(404).json({
+        statusCode: info.statusCode,
+        msg: info.msg,
+      });
+    }
+    req.logIn(user, (error) => {
+      if (error) {
+        console.log(error.message);
+        return next(error);
+      }
+      return res.status(200).json({
+        statusCode: 200,
+        msg: "You logged in successfully",
+      });
+    });
+  })(req, res, next);
+};
+
+// Get logged in account
+const getLoggedInAccount = async (req, res, next) => {
+  if (req.user) {
+    const blogAccount = await usersAccountsServices.getAccountById(req.user.id);
+    return res.status(200).json({
+      statusCode: 200,
+      data: blogAccount,
+    });
+  } else {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `No Account logged in`,
+    });
+  }
+};
+
+// Logout process
+const logout = async (req, res, next) => {
+  if (!req.user) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `No user logged in`,
+    });
+  }
+  req.logout((err) => {
+    if (err) return next(err);
+
+    return res.status(200).json({
+      statusCode: 200,
+      msg: "You successfully logged out!",
+    });
+  });
+};
+
+module.exports = {
+  login,
+  logout,
+  getLoggedInAccount,
+};
