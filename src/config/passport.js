289f183fd9cb98981a6d914a03314a39358c7c97
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 12:35:55 -0300

    Updated passport github and google strategies

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,19 +1,14 @@
 const LocalStrategy = require("passport-local").Strategy;
-
-const BlogAccount = require("../models/BlogAccount");
-const Notification = require("../models/Notification");
-
-const bcrypt = require("bcryptjs");
-
-const { getBlogAccountById } = require("../controllers/blogs");
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GithubStrategy = require("passport-github2").Strategy;
 
+const bcrypt = require("bcryptjs");
+
 require("dotenv").config();
 
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const usersAccountsServices = require("../services/usersAccounts");
+const notificationsServices = require("../services/notifications");
+const emailsServices = require("../services/emails");
 
 module.exports = (passport) => {
   passport.use(
@@ -22,11 +17,10 @@ module.exports = (passport) => {
       async (email, password, done) => {
         try {
           // Match email
-          const accountFound = await BlogAccount.findOne({
-            where: {
-              email,
-            },
-          });
+          const accountFound = await usersAccountsServices.checkEmailExists(
+            email
+          );
+
           if (accountFound) {
             // Match password
             if (accountFound.type === "LOCAL") {
@@ -38,7 +32,9 @@ module.exports = (passport) => {
                     return done(err, null);
                   }
                   if (isMatch) {
-                    const account = await getBlogAccountById(accountFound.id);
+                    const account = await usersAccountsServices.getAccountById(
+                      accountFound.id
+                    );
 
                     if (account.isBanned === true) {
                       return done(null, false, {
@@ -80,18 +76,18 @@ module.exports = (passport) => {
         {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          callbackURL: "/api/auth/google/callback",
+          callbackURL: "/api/users/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
           try {
-            const userFound = await BlogAccount.findOne({
-              where: {
-                email: profile.emails[0].value,
-              },
-            });
+            const userFound = await usersAccountsServices.checkEmailExists(
+              profile.emails[0].value
+            );
 
             if (userFound) {
-              const account = await getBlogAccountById(userFound.id);
+              const account = await usersAccountsServices.getAccountById(
+                userFound.id
+              );
 
               if (account.isBanned === true) {
                 return done(null, false, {
@@ -104,28 +100,25 @@ module.exports = (passport) => {
                 return done(null, account);
               }
             } else {
-              const accountCreated = await BlogAccount.create({
-                email: profile.emails[0].value,
-                type: "GOOGLE",
-              });
+              const accountCreated =
+                await usersAccountsServices.createAccountFromGithubOrGoogle(
+                  profile.emails[0].value,
+                  "GOOGLE"
+                );
+
               if (accountCreated) {
-                const account = await getBlogAccountById(accountCreated.id);
+                const account = await usersAccountsServices.getAccountById(
+                  accountCreated.id
+                );
 
-                const url = `http://localhost:5000/api/account/${account.id}/verify`;
+                const url = `${process.env.URL}/api/users/account/${account.id}/verify`;
 
-                const msg = {
-                  to: account.email,
-                  from: process.env.SENDGRID_SENDER,
-                  subject: "Verify your account",
-                  html: `<html><a href=${url}>${url}</a></html>`,
-                };
+                await emailsServices.sendEmailVerification(url, account.email);
 
-                await sgMail.send(msg);
-
-                await Notification.create({
-                  blogAccountId: accountCreated.id,
-                  text: "Your account was created successfully!",
-                });
+                await notificationsServices.createNotification(
+                  accountCreated.id,
+                  "Your account was created successfully!"
+                );
 
                 return done(null, accountCreated);
               }
@@ -144,18 +137,16 @@ module.exports = (passport) => {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         scope: ["user:email"],
-        callbackURL: "/api/auth/github/callback",
+        callbackURL: "/api/users/auth/github/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
-          const userFound = await BlogAccount.findOne({
-            where: {
-              email: profile.emails[0].value,
-            },
-          });
+          const userFound = await usersAccountsServices.checkEmailExists(
+            profile.emails[0].value
+          );
 
           if (userFound) {
-            const account = await getBlogAccountById(userFound.id);
+            const account = await usersAccountsServices.get(userFound.id);
 
             if (account.isBanned === true) {
               return done(null, false, {
@@ -168,28 +159,26 @@ module.exports = (passport) => {
               return done(null, account);
             }
           } else {
-            const accountCreated = await BlogAccount.create({
-              email: profile.emails[0].value,
-              type: "GITHUB",
-            });
+            const accountCreated =
+              await usersAccountsServices.createAccountFromGithubOrGoogle(
+                profile.emails[0].value,
+                "GITHUB"
+              );
+
             if (accountCreated) {
-              const account = await getBlogAccountById(accountCreated.id);
+              const account = await usersAccountsServices.getAccountById(
+                accountCreated.id
+              );
 
-              const url = `http://localhost:5000/api/account/${account.id}/verify`;
+              const url = `${process.env.URL}/api/users/account/${account.id}/verify`;
 
-              const msg = {
-                to: account.email,
-                from: process.env.SENDGRID_SENDER,
-                subject: "Verify your account",
-                html: `<html><a href=${url}>${url}</a></html>`,
-              };
+              await emailsServices.sendEmailVerification(url, account.email);
 
-              await sgMail.send(msg);
+              await notificationsServices.createNotification(
+                accountCreated.id,
+                "Your account was created successfully!"
+              );
 
-              await Notification.create({
-                blogAccountId: accountCreated.id,
-                text: "Your account was created successfully!",
-              });
               return done(null, accountCreated);
             }
           }
@@ -206,7 +195,7 @@ module.exports = (passport) => {
 
   passport.deserializeUser(async (id, done) => {
     try {
-      const accountFound = await getBlogAccountById(id);
+      const accountFound = await usersAccountsServices.getAccountById(id);
       if (accountFound) {
         done(null, accountFound);
       } else {
