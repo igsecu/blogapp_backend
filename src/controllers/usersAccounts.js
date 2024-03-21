010d5ffeb95e24eb0e22e6a639b33e237f1df0dc
Ignacio Cunial <igsecu@hotmail.com>
2024-03-21 16:46:06 -0300

    Removed functions

--- src/controllers/usersAccounts.js
+++ src/controllers/usersAccounts.js
@@ -115,94 +115,6 @@ const createAccount = async (req, res, next) => {
   }
 };
 
-// Login process
-const login = async (req, res, next) => {
-  if (req.user) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: "An user is already logged in!",
-    });
-  }
-
-  const { email, password } = req.body;
-
-  if (validateEmail(email)) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: validateEmail(email),
-    });
-  }
-
-  if (!password) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: "Password is missing",
-    });
-  }
-
-  passport.authenticate("local", (error, user, info) => {
-    if (error) {
-      return next(error);
-    }
-    if (!user) {
-      if (info.statusCode === 400) {
-        return res.status(400).json({
-          statusCode: info.statusCode,
-          msg: info.msg,
-        });
-      }
-      return res.status(404).json({
-        statusCode: info.statusCode,
-        msg: info.msg,
-      });
-    }
-    req.logIn(user, (error) => {
-      if (error) {
-        console.log(error.message);
-        return next(error);
-      }
-      return res.status(200).json({
-        statusCode: 200,
-        msg: "You logged in successfully",
-      });
-    });
-  })(req, res, next);
-};
-
-// Get logged in account
-const getLoggedInAccount = async (req, res, next) => {
-  if (req.user && req.user.isAdmin === false) {
-    const blogAccount = await usersAccountsServices.getAccountById(req.user.id);
-    return res.status(200).json({
-      statusCode: 200,
-      data: blogAccount,
-    });
-  } else {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: `No User Account logged in`,
-    });
-  }
-};
-
-// Logout process
-const logout = async (req, res, next) => {
-  if (!req.user) {
-    return res.status(400).json({
-      statusCode: 400,
-      msg: `No user logged in`,
-    });
-  }
-  req.logout((err) => {
-    if (err) return next(err);
-
-    return res.status(200).json({
-      statusCode: 200,
-      msg: "You successfully logged out!",
-    });
-  });
-};
-
 // Google Callback
 const googleCallback = async (req, res, next) => {
   if (req.user) {
@@ -318,9 +230,6 @@ const verifyAccount = async (req, res, next) => {
 
 module.exports = {
   createAccount,
-  login,
-  getLoggedInAccount,
-  logout,
   googleCallback,
   githubCallback,
   verifyAccount,
