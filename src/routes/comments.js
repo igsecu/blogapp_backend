efbc49694fa2cf6b5e0cccaaf2b0b6bb8720a672
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 16:32:59 -0300

    Added POST /comment route

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const usersCommentsController = require("../controllers/usersBlogs");
+const usersCommentsController = require("../controllers/usersComments");
 
 const { ensureAuthenticatedUser } = require("../utils/index");
+
+// Create new comment
+router.post(
+  "/comment",
+  ensureAuthenticatedUser,
+  usersCommentsController.createComment
+);
 
 module.exports = router;
