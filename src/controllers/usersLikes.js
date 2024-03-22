2fbd0fc2357f06e60a6248665e599c3878fcb426
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 16:55:00 -0300

    Created deleteLike controller

--- src/controllers/usersLikes.js
+++ src/controllers/usersLikes.js
@@ -78,4 +78,69 @@ const createLike = async (req, res, next) => {
   }
 };
 
-module.exports = { createLike };
+// Delete like
+const deleteLike = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    if (!validateId(id)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `ID: ${id} - Invalid format!`,
+      });
+    }
+
+    const post = await usersPostsServices.getPostById(id);
+
+    if (!post) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Post with ID: ${id} not found!`,
+      });
+    }
+
+    if (post.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This post is banned! You can not dislike it...",
+      });
+    }
+
+    if (post.blog.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "The blog of the post is banned! You can not dislike it...",
+      });
+    }
+
+    if (post.blog.account.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "The account of the post is banned! You can not dislike it...",
+      });
+    }
+
+    const likeFound = await usersLikesServices.checkLikeExists(req.user.id, id);
+
+    if (!likeFound) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "You can not dislike a post that you did not like!",
+      });
+    }
+
+    const likeDeleted = await usersLikesServices.deleteLike(likeFound.id, id);
+
+    if (likeDeleted) {
+      return res.status(200).json({
+        statusCode: 201,
+        msg: `You disliked the post: ${post.title}`,
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    return next("Error trying to dislike a post");
+  }
+};
+
+module.exports = { createLike, deleteLike };
