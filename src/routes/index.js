cef8ea55f0eddf33c57ea066dd556c6016aea2f7
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 16:17:48 -0300

    Imported comments router and specified comments router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,14 @@ const usersRouter = require("./users");
 const loginRouter = require("./login");
 const adminRouter = require("./admin");
 const postsRouter = require("./posts");
+const commentsRouter = require("./comments");
 
 // Specify routers root routes
 router.use("/", loginRouter);
 router.use("/users", usersRouter);
 router.use("/users", blogsRouter);
 router.use("/users", postsRouter);
+router.use("/comments", commentsRouter);
 router.use("/admin", adminRouter);
 
 module.exports = router;
