c96602e212ca34537bf5da19a7261ca7d1acd2dd
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 17:13:55 -0300

    Added PUT /account/:id/banned/true route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const adminAccountsController = require("../controllers/adminAccounts");
+const { ensureAuthenticatedAdmin } = require("../utils");
 
 // Create new admin account
 router.post("/account", adminAccountsController.createAccount);
+// Ban user account
+router.put(
+  "/account/:id/banned/true",
+  ensureAuthenticatedAdmin,
+  adminAccountsController.updateAccount
+);
 
 module.exports = router;
