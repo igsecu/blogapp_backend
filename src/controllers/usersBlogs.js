e719b933fb4039a6c651bbfe80ec2c80d0b0d400
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 14:48:18 -0300

    Created updateBlogName controller

--- src/controllers/usersBlogs.js
+++ src/controllers/usersBlogs.js
@@ -8,6 +8,7 @@ const {
   validateTitle,
 } = require("../utils/index");
 
+// Create new blog
 const createBlog = async (req, res, next) => {
   const { name } = req.body;
 
@@ -48,6 +49,79 @@ const createBlog = async (req, res, next) => {
   }
 };
 
+// Update blog name
+const updateBlogName = async (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    if (!validateId(id)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `ID: ${id} - Invalid format!`,
+      });
+    }
+
+    const blog = await usersBlogsServices.getBlogById(id);
+
+    if (!blog) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Blog with ID: ${id} not found!`,
+      });
+    }
+
+    if (blog.account.id !== req.user.id) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "You can not update a blog that is not yours!",
+      });
+    }
+
+    if (blog.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This blog is banned! You can not update its name...",
+      });
+    }
+
+    if (validateName(name)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validateName(name),
+      });
+    }
+
+    const blogFound = await usersBlogsServices.checkBlogExists(name);
+
+    if (blogFound) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `Blog with name: ${name} exists! Try with another one...`,
+      });
+    }
+
+    const updatedBlog = await usersBlogsServices.updateBlogName(id, name);
+
+    if (updatedBlog) {
+      await notificationsServices.createNotification(
+        req.user.id,
+        "The name of the blog was updated successfully!"
+      );
+
+      return res.status(200).json({
+        statusCode: 200,
+        msg: "Blog updated successfully!",
+        data: updatedBlog,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return next("Error trying to update blog name");
+  }
+};
+
 module.exports = {
   createBlog,
+  updateBlogName,
 };
