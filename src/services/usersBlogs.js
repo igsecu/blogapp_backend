39e0e29f12b8aae5267646a92f117cb1e9082726
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 16:06:54 -0300

    Updated deleteBlog service -> delete posts and images from blog

--- src/services/usersBlogs.js
+++ src/services/usersBlogs.js
@@ -1,7 +1,10 @@
 const Blog = require("../models/Blog");
 const BlogAccount = require("../models/BlogAccount");
+const Post = require("../models/Post");
 
 const { Op } = require("sequelize");
+
+const { deleteImage } = require("../utils/cloudinary");
 
 // Get blog by id
 const getBlogById = async (id) => {
@@ -94,27 +97,60 @@ const updateBlogName = async (id, name) => {
   }
 };
 
+// Get Blog Posts
+const getBlogPosts = async (id) => {
+  const array = [];
+  try {
+    const results = await Post.findAll({
+      include: {
+        model: Blog,
+        where: {
+          id,
+        },
+      },
+    });
+
+    if (results) {
+      results.forEach((r) => {
+        array.push({
+          id: r.id,
+          title: r.title,
+          text: r.text,
+          isBanned: r.isBanned,
+          readers: r.readers,
+          comments_number: r.comments_number,
+          likes_number: r.likes_number,
+          image: r.image,
+        });
+      });
+    }
+
+    return array;
+  } catch (error) {
+    throw new Error("Error trying to get all blog posts");
+  }
+};
+
 // Delete blog
 const deleteBlog = async (id) => {
   try {
     const blog = await getBlogById(id);
 
-    /* 
-      const results = await getBlogPosts(id);
-  
-      for (let r of results) {
-        const post = await Post.findByPk(r.id);
-  
-        if (post.image_id !== null) {
-          await deleteImage(post.image_id);
-        }
-  
-        await Post.destroy({
-          where: {
-            id: r.id,
-          },
-        });
-      } */
+    const results = await getBlogPosts(id);
+
+    for (let r of results) {
+      const post = await Post.findByPk(r.id);
+
+      if (post.image_id !== null) {
+        await deleteImage(post.image_id);
+      }
+
+      await Post.destroy({
+        where: {
+          id: r.id,
+        },
+      });
+    }
 
     const blogDeleted = await Blog.destroy({
       where: {
