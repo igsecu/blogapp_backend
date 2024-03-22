1309866f1e19c573c52a371e4e37a6bc984f415c
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 15:27:47 -0300

    Added POST /post route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
-const passport = require("passport");
+const fileUpload = require("express-fileupload");
 
 const usersPostsController = require("../controllers/usersPosts");
 
 const { ensureAuthenticatedUser } = require("../utils/index");
+
+// Create new post
+router.post(
+  "/post",
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: `${__dirname}/../../uploads`,
+  }),
+  ensureAuthenticatedUser,
+  usersPostsController.createPost
+);
 
 module.exports = router;
