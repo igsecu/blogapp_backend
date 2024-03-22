e0f43761459d162d7adc5223ba749d34ebfe24ee
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 17:15:23 -0300

    Created banAccount service

--- src/services/adminAccounts.js
+++ src/services/adminAccounts.js
@@ -15,6 +15,31 @@ const createAccount = async (hash, email) => {
   }
 };
 
+// Ban account
+const banAccount = async (id) => {
+  try {
+    const updatedAccount = await BlogAccount.update(
+      {
+        isBanned: true,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    console.log(updatedAccount);
+
+    if (updatedAccount) {
+      return updatedAccount;
+    }
+  } catch (error) {
+    throw new Error("Error trying to ban an account");
+  }
+};
+
 module.exports = {
   createAccount,
+  banAccount,
 };
