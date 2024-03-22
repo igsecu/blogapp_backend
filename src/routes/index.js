30819b10184e19ef418d56b01a94c64bb1bf26fb
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 16:35:30 -0300

    Imported likes router and specified likes router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,15 @@ const loginRouter = require("./login");
 const adminRouter = require("./admin");
 const postsRouter = require("./posts");
 const commentsRouter = require("./comments");
+const likesRouter = require("./likes");
 
 // Specify routers root routes
 router.use("/", loginRouter);
 router.use("/users", usersRouter);
 router.use("/users", blogsRouter);
 router.use("/users", postsRouter);
-router.use("/comments", commentsRouter);
+router.use("/users", commentsRouter);
+router.use("/users", likesRouter);
 router.use("/admin", adminRouter);
 
 module.exports = router;
