fe0ae02ac5d7d119ca810e64eeda3407dc54dd5d
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 17:44:56 -0300

    Created getPosts service

--- src/services/adminPosts.js
+++ src/services/adminPosts.js
@@ -1,4 +1,6 @@
 const Post = require("../models/Post");
+const Blog = require("../models/Blog");
+const BlogAccount = require("../models/BlogAccount");
 
 // Ban post
 const banPost = async (id) => {
@@ -44,7 +46,80 @@ const notBanPost = async (id) => {
   }
 };
 
+// Get all posts
+const getPosts = async (page, limit) => {
+  const results = [];
+  try {
+    const dbResults = await Post.findAndCountAll({
+      attributes: [
+        "id",
+        "title",
+        "text",
+        "isBanned",
+        "readers",
+        "comments_number",
+        "likes_number",
+        "image",
+      ],
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "name", "isBanned"],
+          include: {
+            model: BlogAccount,
+            attributes: ["id", "username", "email", "isBanned"],
+          },
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (dbResults.count === 0) {
+      return false;
+    }
+
+    if (dbResults.rows.length > 0) {
+      dbResults.rows.forEach((r) => {
+        results.push({
+          id: r.id,
+          title: r.title,
+          text: r.text,
+          isBanned: r.isBanned,
+          readers: r.readers,
+          likes: r.likes_number,
+          comments: r.comments_number,
+          image: r.image,
+          blog: {
+            id: r.blog.id,
+            name: r.blog.name,
+            isBanned: r.blog.isBanned,
+            account: {
+              id: r.blog.blogAccount.id,
+              username: r.blog.blogAccount.username,
+              image: r.blog.blogAccount.image,
+              isBanned: r.blog.blogAccount.isBanned,
+            },
+          },
+        });
+      });
+      return {
+        totalResults: dbResults.count,
+        totalPages: Math.ceil(dbResults.count / limit),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    throw new Error("Error trying to get all posts");
+  }
+};
+
 module.exports = {
   banPost,
   notBanPost,
+  getPosts,
 };
