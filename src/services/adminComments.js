7d788a7afae0ffb62cd7141eccbb08fc3e31631e
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 18:01:32 -0300

    Created getComments service

--- src/services/adminComments.js
+++ src/services/adminComments.js
@@ -1,4 +1,6 @@
 const Comment = require("../models/Comment");
+const BlogAccount = require("../models/BlogAccount");
+const Post = require("../models/Post");
 
 // Ban comment
 const banComment = async (id) => {
@@ -44,7 +46,64 @@ const notBanComment = async (id) => {
   }
 };
 
+// Get comments
+const getComments = async (page, limit) => {
+  const results = [];
+  try {
+    const dbResults = await Comment.findAndCountAll({
+      attributes: ["id", "text", "isBanned"],
+      include: [
+        {
+          model: BlogAccount,
+          attributes: ["id", "email", "username"],
+        },
+        {
+          model: Post,
+          attributes: ["id", "title"],
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (dbResults.count === 0) {
+      return false;
+    }
+
+    if (dbResults.rows.length > 0) {
+      dbResults.rows.forEach((r) => {
+        results.push({
+          id: r.id,
+          text: r.text,
+          isBanned: r.isBanned,
+          account: {
+            id: r.blogAccount.id,
+            email: r.blogAccount.email,
+            username: r.blogAccount.username,
+          },
+          post: {
+            id: r.post.id,
+            title: r.post.title,
+          },
+        });
+      });
+      return {
+        totalResults: dbResults.count,
+        totalPages: Math.ceil(dbResults.count / limit),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    throw new Error("Error trying to get all comments");
+  }
+};
+
 module.exports = {
   banComment,
   notBanComment,
+  getComments,
 };
