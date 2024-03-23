c2bddba87b2e142add87560df58b952a02928b84
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 18:01:20 -0300

    Created getComments controller

--- src/controllers/adminComments.js
+++ src/controllers/adminComments.js
@@ -1,7 +1,7 @@
 const adminCommentsServices = require("../services/adminComments");
 const usersCommentsServices = require("../services/usersComments");
 
-const { validateId } = require("../utils/index");
+const { validateId, validatePage, validateLimit } = require("../utils/index");
 
 // Ban comment
 const banComment = async (req, res, next) => {
@@ -71,7 +71,65 @@ const notBanComment = async (req, res, next) => {
   }
 };
 
+// Get comments
+const getComments = async (req, res, next) => {
+  const { page, limit } = req.query;
+  try {
+    if (page) {
+      if (validatePage(page)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "Page must be a number",
+        });
+      }
+
+      if (parseInt(page) === 0) {
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Page ${page} not found!`,
+        });
+      }
+    }
+
+    if (limit) {
+      if (validateLimit(limit)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "Limit must be a number",
+        });
+      }
+    }
+
+    const comments = await adminCommentsServices.getComments(
+      page ? page : 1,
+      limit ? limit : 10
+    );
+
+    if (!comments) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: "No comments saved in DB!",
+      });
+    }
+
+    if (!comments.data.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Page ${page} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      ...comments,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   banComment,
   notBanComment,
+  getComments,
 };
