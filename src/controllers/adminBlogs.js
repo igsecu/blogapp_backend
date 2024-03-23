219a78a6608ea148f579a8b2b1c553e2af726b05
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 17:34:51 -0300

    Created getFilteredBlogs controller

--- src/controllers/adminBlogs.js
+++ src/controllers/adminBlogs.js
@@ -105,8 +105,6 @@ const getBlogs = async (req, res, next) => {
       limit ? limit : 10
     );
 
-    console.log(blogs);
-
     if (!blogs) {
       return res.status(404).json({
         statusCode: 404,
@@ -131,8 +129,75 @@ const getBlogs = async (req, res, next) => {
   }
 };
 
+// Get Filtered Blogs
+const getFilteredBlogs = async (req, res, next) => {
+  const { page, limit, name } = req.query;
+  try {
+    if (!name) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "Name query is missing",
+      });
+    }
+
+    if (page) {
+      if (validatePage(page)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "Page must be a number",
+        });
+      }
+
+      if (parseInt(page) === 0) {
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Page ${page} not found!`,
+        });
+      }
+    }
+
+    if (limit) {
+      if (validateLimit(limit)) {
+        return res.status(400).json({
+          statusCode: 400,
+          msg: "Limit must be a number",
+        });
+      }
+    }
+
+    const blogs = await adminBlogsServices.getFilteredBlogs(
+      name,
+      page ? page : 1,
+      limit ? limit : 10
+    );
+
+    if (!blogs) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `No blogs with name: ${name} found!`,
+      });
+    }
+
+    if (!blogs.data.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Page ${page} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      ...blogs,
+    });
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+};
+
 module.exports = {
   banBlog,
   notBanBlog,
   getBlogs,
+  getFilteredBlogs,
 };
