c3923eec5202a81e06959a6c1ce6e734f05ce30a
Ignacio Cunial <igsecu@hotmail.com>
2024-03-22 22:12:43 -0300

    Created getBannedAccounts and getNotBannedAccounts services

--- src/services/adminAccounts.js
+++ src/services/adminAccounts.js
@@ -115,9 +115,121 @@ const getAccounts = async (page, limit) => {
   }
 };
 
+// Get banned accounts
+const getBannedAccounts = async (page, limit) => {
+  const results = [];
+  try {
+    const accounts = await BlogAccount.findAndCountAll({
+      attributes: [
+        "id",
+        "email",
+        "username",
+        "isBanned",
+        "isVerified",
+        "isAdmin",
+        "image",
+      ],
+      where: {
+        isAdmin: false,
+        isBanned: true,
+      },
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (accounts.count === 0) {
+      return false;
+    }
+
+    if (accounts.rows.length > 0) {
+      accounts.rows.forEach((r) => {
+        results.push({
+          id: r.id,
+          email: r.email,
+          username: r.username,
+          isBanned: r.isBanned,
+          isVerified: r.isVerified,
+          isAdmin: r.isAdmin,
+          image: r.image,
+        });
+      });
+
+      return {
+        totalResults: accounts.count,
+        totalPages: Math.ceil(accounts.count / limit),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to get banned accounts");
+  }
+};
+
+// Get not banned accounts
+const getNotBannedAccounts = async (page, limit) => {
+  const results = [];
+  try {
+    const accounts = await BlogAccount.findAndCountAll({
+      attributes: [
+        "id",
+        "email",
+        "username",
+        "isBanned",
+        "isVerified",
+        "isAdmin",
+        "image",
+      ],
+      where: {
+        isAdmin: false,
+        isBanned: false,
+      },
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (accounts.count === 0) {
+      return false;
+    }
+
+    if (accounts.rows.length > 0) {
+      accounts.rows.forEach((r) => {
+        results.push({
+          id: r.id,
+          email: r.email,
+          username: r.username,
+          isBanned: r.isBanned,
+          isVerified: r.isVerified,
+          isAdmin: r.isAdmin,
+          image: r.image,
+        });
+      });
+
+      return {
+        totalResults: accounts.count,
+        totalPages: Math.ceil(accounts.count / limit),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to get not banned accounts");
+  }
+};
+
 module.exports = {
   createAccount,
   banAccount,
   notBanAccount,
   getAccounts,
+  getBannedAccounts,
+  getNotBannedAccounts,
 };
