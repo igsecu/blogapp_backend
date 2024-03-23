861e202423981d8fa3d88610a1ee56c367cab0cc
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 17:51:20 -0300

    Created getFilteredPost service

--- src/services/adminPosts.js
+++ src/services/adminPosts.js
@@ -2,6 +2,8 @@ const Post = require("../models/Post");
 const Blog = require("../models/Blog");
 const BlogAccount = require("../models/BlogAccount");
 
+const { Op } = require("sequelize");
+
 // Ban post
 const banPost = async (id) => {
   try {
@@ -118,8 +120,91 @@ const getPosts = async (page, limit) => {
   }
 };
 
+// Get filtered posts
+const getFilteredPosts = async (text, page, limit) => {
+  const results = [];
+  try {
+    const dbResults = await Post.findAndCountAll({
+      attributes: [
+        "id",
+        "title",
+        "text",
+        "isBanned",
+        "readers",
+        "comments_number",
+        "likes_number",
+        "image",
+      ],
+      include: [
+        {
+          model: Blog,
+          attributes: ["id", "name", "isBanned"],
+          include: {
+            model: BlogAccount,
+            attributes: ["id", "username", "email", "isBanned"],
+          },
+        },
+      ],
+      where: {
+        [Op.or]: {
+          title: {
+            [Op.iLike]: `%${text}%`,
+          },
+          text: {
+            [Op.iLike]: `%${text}%`,
+          },
+        },
+      },
+      order: [["createdAt", "DESC"]],
+      limit,
+      offset: page * limit - limit,
+    });
+
+    if (dbResults.count === 0) {
+      return false;
+    }
+
+    if (dbResults.rows.length > 0) {
+      dbResults.rows.forEach((r) => {
+        results.push({
+          id: r.id,
+          title: r.title,
+          text: r.text,
+          isBanned: r.isBanned,
+          readers: r.readers,
+          likes: r.likes_number,
+          comments: r.comments_number,
+          image: r.image,
+          blog: {
+            id: r.blog.id,
+            name: r.blog.name,
+            isBanned: r.blog.isBanned,
+            account: {
+              id: r.blog.blogAccount.id,
+              username: r.blog.blogAccount.username,
+              image: r.blog.blogAccount.image,
+              isBanned: r.blog.blogAccount.isBanned,
+            },
+          },
+        });
+      });
+      return {
+        totalResults: dbResults.count,
+        totalPages: Math.ceil(dbResults.count / limit),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    throw new Error("Error trying to get filtered posts");
+  }
+};
+
 module.exports = {
   banPost,
   notBanPost,
   getPosts,
+  getFilteredPosts,
 };
