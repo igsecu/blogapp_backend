d3d44173bc9e3b7d9bbd37f2253e76a5ee1d30da
Ignacio Cunial <igsecu@hotmail.com>
2024-03-23 20:36:33 -0300

    Added GET /blogs /blogs/filter /blogs/account/:id /blogs/account/auth routes

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -7,6 +7,26 @@ const usersBlogsController = require("../controllers/usersBlogs");
 
 const { ensureAuthenticatedUser } = require("../utils/index");
 
+// Get logged in account blogs
+router.get(
+  "/blogs/account/auth",
+  ensureAuthenticatedUser,
+  usersBlogsController.getOwnBlogs
+);
+// Get account blogs
+router.get(
+  "/blogs/account/:id",
+  ensureAuthenticatedUser,
+  usersBlogsController.getAccountBlogs
+);
+// Get filtered blogs by name
+router.get(
+  "/blogs/filter",
+  ensureAuthenticatedUser,
+  usersBlogsController.getFilteredBlogs
+);
+// Get all blogs
+router.get("/blogs", ensureAuthenticatedUser, usersBlogsController.getBlogs);
 // Create new blog
 router.post("/blog", ensureAuthenticatedUser, usersBlogsController.createBlog);
 // Update blog name
